48ca5013bf1c86bc648d4476a0d21cc148864e61
Filip Taczkowski <filip.taczkowski@gmail.com>
2019-10-03 18:34:08 +0200

    Add js functions displayResult and getMoveName

--- js/script.js
+++ js/script.js
@@ -1,23 +1,66 @@
+function getMoveName(argMoveId){
+    console.log('Move ID: ' + argMoveId);
+
+    if(argMoveId == 1){
+      return 'rock';
+    } 
+    if (argMoveId == 2) {
+        return 'paper';
+    } 
+    if (argMoveId == 3) {
+        return 'scissors';
+    } 
+  
+    printMessage('This move does not exist: ' + argMoveId + '.');
+    return 'unknown move!';
+}
+
+function displayResult(argComputerMove, argPlayerMove) {
+    console.log('Moves: ', argComputerMove, argPlayerMove);
+    
+    
+    if (argPlayerMove == argComputerMove) {
+        printMessage('Draw!');
+    } else if (argPlayerMove == 'rock' && argComputerMove == 'scissors') {
+        printMessage('You win!');
+    } else if (argPlayerMove == 'rock' && argComputerMove == 'paper') {
+        printMessage('You lose!');
+    } else if (argPlayerMove == 'scissors' && argComputerMove == 'paper') {
+        printMessage('You win!');
+    } else if (argPlayerMove == 'scissors' && argComputerMove == 'rock') {
+        printMessage('You lose!');
+    } else if (argPlayerMove == 'paper' && argComputerMove == 'rock') {
+        printMessage('You win!');
+    } else if (argPlayerMove == 'paper' && argComputerMove == 'scissors') {
+        printMessage('You lose!');
+    } else if (argPlayerMove == 'Unkown move') {
+        printMessage('Unkown move! Please select availiable moves (1 , 2 , 3 )');
+    }
+}
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Radnom number: ' + randomNumber);
 
-let computerMove = 'Unknown move';
+let computerMove = getMoveName(randomNumber);
 
-if (randomNumber == 1) {
-    computerMove = 'rock';
+/*
+ if (randomNumber == 1) {
+computerMove = 'rock';
 } else if (randomNumber == 2) {
     computerMove = 'paper';
 } else if (randomNumber == 3) {
     computerMove = 'scissors';
-}
+} 
+*/
 
 let playerInput = prompt('Choose your move! 1: rock, 2: paper, 3: scissors');
 
 console.log('Player input: ' + playerInput);
 
-let playerMove = 'Unkown move';
+let playerMove = getMoveName(playerInput);
 
+/*
 if (playerInput == 1) {
     playerMove = 'rock';
 } else if (playerInput == 2) {
@@ -25,7 +68,9 @@ if (playerInput == 1) {
 } else if (playerInput == 3) {
     playerMove = 'scissors';
 }
+*/
 
+/*
 if (playerMove == computerMove) {
     printMessage('Draw!');
 } else if (playerMove == 'rock' && computerMove == 'scissors') {
@@ -43,5 +88,7 @@ if (playerMove == computerMove) {
 } else if (playerMove == 'Unkown move') {
     printMessage('Unkown move! Please select availiable moves (1 , 2 , 3 )');
 }
+*/
 
+displayResult(computerMove, playerMove);
 printMessage('You played: ' + playerMove + '! Opponent played: ' + computerMove + '!');
